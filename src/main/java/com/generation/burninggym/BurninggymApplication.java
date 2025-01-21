0d3f5987bed524c60b655ca001ae34c5e6cf8873
GustavoCorrea10 <guganow11@gmail.com>
2025-01-21 10:39:05 -0300

    iniciando o projeto burning gym

---
+++ src/main/java/com/generation/burninggym/BurninggymApplication.java
@@ -0,0 +1,13 @@
+package com.generation.burninggym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurninggymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurninggymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/burninggym/BurninggymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.burninggym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurninggymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
